b7e7c8dd8c14a4002f0df76efd991a35ce23026c
pspiagicw <pspiagicw@gmail.com>
2024-08-10 15:16:44 +0530

    15 current  2024-08-10 15:16:42  24.05.20240806.21cc704  6.6.44                          *

--- home.nix
+++ home.nix
@@ -36,7 +36,7 @@
   programs.password-store = {
     enable = true;
     settings = {
-      PASSWORD_STORE_DIR = "$XDG_DATA_HOME/.password-store";
+      PASSWORD_STORE_DIR = "/home/pspiagicw/.password-store";
     };
   };
   programs.gpg.enable = true;
