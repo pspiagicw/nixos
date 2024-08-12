bc3b897f9bb06a733e14e6ba8d048d9cd58f778b
pspiagicw <pspiagicw@gmail.com>
2024-08-12 13:22:32 +0530

    34 current  2024-08-12 13:22:28  24.05.20240810.a781ff3  6.6.44                          *

--- configuration.nix
+++ configuration.nix
@@ -97,7 +97,7 @@
   ];
 
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
+  networking.firewall.allowedTCPPorts = [1313];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
