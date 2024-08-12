85acfbded2c8549e75182391da7c1f7ac823dd92
pspiagicw <pspiagicw@gmail.com>
2024-08-12 12:07:10 +0530

    33 current  2024-08-12 12:07:06  24.05.20240810.a781ff3  6.6.44                          *

--- groom.nix
+++ groom.nix
@@ -5,7 +5,7 @@ pkgs.buildGoModule rec {
     owner = "pspiagicw";
     repo = "groom";
     rev = "3fd54797db3a614cbc3fa43f3e29a152aa01ffde";
-    sha256 = "29JW1yi8tcjxnv68Bg2TRllk6rujgkC6i8P72nti/I0=";
+    sha256 = "O37ciBvtbuZX3KNVSg7ZG02o+b0PCQ2K8hJoyo2hPsY=";
   };
 
   vendorHash = "sha256-r0ttcRQscZFrY69+EBFRLAlWgmKUXBFanQtX1OWvm48=";
